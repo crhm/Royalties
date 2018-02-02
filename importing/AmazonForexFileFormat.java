86cc26090c8c28e371fd22a2a3c0ad376d3e3091
crhm <33059858+crhm@users.noreply.github.com>
2018-02-02 15:59:56 +0000

    Added comments on the last remaining importing classes without them, and cleaned them up

--- importing/AmazonForexFileFormat.java
+++ importing/AmazonForexFileFormat.java
@@ -10,14 +10,32 @@ import java.util.HashMap;
 
 import main.SalesHistory;
 
+/**Class that represents the format for FX rates and payment data files from Amazon channel and performs the import of the data found
+ *  in such files, through method importData(). It is an implementation of the IFileFormat interface.
+ * 
+ * @author crhm
+ *
+ */
 public class AmazonForexFileFormat implements IFileFormat {
 
 	private String monthAndYear = "";
 	private HashMap<String, Double> listForex = new HashMap<String, Double>();
 	
+	/** Imports the FX data found in the Amazon raw FX data file designated by the argument passed.
+	 * <br>Expects to find a date of format '2017-01-23' in the first cell of the second line of the csv.
+	 * <br>Expects relevant lines to be only even numbered lines.
+	 * <br>Expects to find the first relevant line at the second line of the csv.
+	 * <br>Expects relevant lines to be more than 15 characters long.
+	 * <br>Will rewrite FX rates for existing currencies also present in this file, 
+	 *  if any are present in the database for this month and year.
+	 * <br>Does not import the FX rate provided by Amazon but the exact FX rate given the amount due in foreign currency and the
+	 *  amount that was actually paid to PLP in dollars.
+	 * @param path (from src folder) + name + extension of file to be read and imported
+	 */
 	@Override
 	public void importData(String filePath) {
 		try {
+			//Reads the file
 			BufferedReader br = new BufferedReader(new FileReader(filePath));
 			StringBuilder lines = new StringBuilder();
 			String line = "";
@@ -25,12 +43,14 @@ public class AmazonForexFileFormat implements IFileFormat {
 				line = br.readLine();
 				lines.append(line + "\n");
 			}
-			String temp = lines.toString();
+			br.close();
 			
 			// Places each line as an element in an array of Strings
+			String temp = lines.toString();
 			String[] allLines = temp.split("\n");
-			br.close();
-			int counter = 1;
+			
+			//Obtains the date to give to all sales from the cell in the second line and first column of the csv
+			//And formats it into the expected format.
 			SimpleDateFormat oldFormat = new SimpleDateFormat("yyyy-MM-dd");
 			SimpleDateFormat newFormat = new SimpleDateFormat("MMM yyyy");
 			Date date = null;
@@ -39,33 +59,61 @@ public class AmazonForexFileFormat implements IFileFormat {
 			try {
 				date = oldFormat.parse(oldDate);
 			} catch (ParseException e) {
-				// TODO Auto-generated catch block
+				System.out.println("There was parsing the date in the first cell of the second line of the csv.\n" 
+						+ " A date of the format '2017-01-23' was expected.");
 				e.printStackTrace();
 			}
 			this.monthAndYear = newFormat.format(date);
+			
+			//Parses data for each currency and places it in class variable listForex by calling importForex() on each currency line of csv
+			//Considers that the first line of currencies is the second line of csv.
+			//Stops if counter reaches the total number of lines of csv, or if the line is shorter than 15 characters,
+			//so that it doesn't try to parse the summary lines below the last currency line.
+			//Skips every other line to disregard non-currency lines
+			int counter = 1;
 			while (counter< allLines.length && allLines[counter].length() > 15) {
 				importForex(allLines[counter]);
 				counter = counter + 2;
 			}
+			
+			//Places the imported data in the database,
+			//making sure not to replace the existing list of FX rates for this month and year if there is one.
+			//It does however update the FX rate value for currencies that are already in the database for this month.
 			if (SalesHistory.get().getHistoricalForex().get(monthAndYear) != null) {
 				HashMap<String, Double> existingList = SalesHistory.get().getHistoricalForex().get(monthAndYear);
-				for (String s : existingList.keySet()) { //To ensure no Forex already existing but missing from this one is deleted.
+				for (String s : existingList.keySet()) {
 					listForex.put(s, existingList.get(s));
 				}	
 			}
 			SalesHistory.get().addHistoricalForex(monthAndYear, listForex);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			System.out.println("There was a problem importing this file.");
 			e.printStackTrace();
 		}
 	}
 
+	/**Parses a currency line and places the currency and its associated FX rate into the class variable listForex.
+	 * 
+	 * @param line String csv line to be parsed for currency and FX rate information.
+	 */
 	private void importForex(String line) {
+		//Divides line into its individual cells by splitting on commas that are not within quotes
+		//And trims all leading and trailing whitespace from each value.
 		String[] lineDivided = line.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)", -1);
+		int counter = 0;
+		for (String s : lineDivided) {
+			lineDivided[counter] = s.trim();
+			counter++;
+		}
+		
+		//Uses the value of the 8th cell to find currency
+		String currency = lineDivided[7];
+		
+		//Sets the exchange rate as the division of amount paid to PLP in US dollars and the amount owed to PLP in the foreign currency,
+		//thus obtaining a more exact measure than their listed FX rate.
 		double exchangeRate = 0;
-		String currency = "";
 		exchangeRate = Double.parseDouble(lineDivided[15]) / Double.parseDouble(lineDivided[8]);
-		currency = lineDivided[7];
+		
 		this.listForex.put(currency, exchangeRate);
 	}
 }
--- importing/AppleForexFileFormat.java
+++ importing/AppleForexFileFormat.java
@@ -10,14 +10,31 @@ import java.util.HashMap;
 
 import main.SalesHistory;
 
+/**Class that represents the format for FX rates and payment data files from Apple channel and performs the import of the data found
+ *  in such files, through method importData(). It is an implementation of the IFileFormat interface.
+ * 
+ * @author crhm
+ *
+ */
 public class AppleForexFileFormat implements IFileFormat {
 
 	private String monthAndYear = "";
 	private HashMap<String, Double> listForex = new HashMap<String, Double>();
 	
+	/** Imports the FX data found in the Amazon raw FX data file designated by the argument passed.
+	 * <br>Expects to find a date of format 'October, 2017' in the first cell of the first line of the csv.
+	 * <br>Expects to find the first relevant line at the fifth line of the csv.
+	 * <br>Expects relevant lines to be more than 15 characters long.
+	 * <br>Will rewrite FX rates for existing currencies also present in this file, 
+	 *  if any are present in the database for this month and year.
+	 * <br>Does not import the FX rate provided by Apple but the exact FX rate given the amount due in foreign currency and the
+	 *  amount that was actually paid to PLP in dollars.
+	 * @param path (from src folder) + name + extension of file to be read and imported
+	 */
 	@Override
 	public void importData(String filePath) {
 		try {
+			//Reads the file
 			BufferedReader br = new BufferedReader(new FileReader(filePath));
 			StringBuilder lines = new StringBuilder();
 			String line = "";
@@ -25,12 +42,14 @@ public class AppleForexFileFormat implements IFileFormat {
 				line = br.readLine();
 				lines.append(line + "\n");
 			}
-			String temp = lines.toString();
+			br.close();
 			
 			// Places each line as an element in an array of Strings
+			String temp = lines.toString();
 			String[] allLines = temp.split("\n");
-			br.close();
-			int counter = 4;
+			
+			//Obtains the date to give to all sales from the cell in the first line and first column of the csv
+			//And formats it into the expected format.
 			String[] firstLine = allLines[0].split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)", -1);
 			String[] title = firstLine[0].split("\\(");
 			SimpleDateFormat oldFormat = new SimpleDateFormat("MMMMM, yyyy");
@@ -39,34 +58,62 @@ public class AppleForexFileFormat implements IFileFormat {
 			try {
 				date = oldFormat.parse(title[1].replaceAll("[()]", ""));
 			} catch (ParseException e) {
-				// TODO Auto-generated catch block
+				System.out.println("There was parsing the date in the first cell of the first line of the csv.\n" 
+						+ " A date of the format 'October, 2017' was expected.");
 				e.printStackTrace();
 			}
 			this.monthAndYear = newFormat.format(date);
+			
+			//Parses data for each currency and places it in class variable listForex by calling importForex() on each currency line of csv
+			//Considers that the first line of currencies is the fifth line of csv.
+			//Stops if counter reaches the total number of lines of csv, or if the line is shorter than 15 characters,
+			//so that it doesn't try to parse the summary lines below the last currency line.
+			int counter = 4;
 			while (counter< allLines.length && allLines[counter].length() > 15) {
 				importForex(allLines[counter]);
 				counter++;
 			}
+			
+			//Places the imported data in the database,
+			//making sure not to replace the existing list of FX rates for this month and year if there is one.
+			//It does however update the FX rate value for currencies that are already in the database for this month.
 			if (SalesHistory.get().getHistoricalForex().get(monthAndYear) != null) {
 				HashMap<String, Double> existingList = SalesHistory.get().getHistoricalForex().get(monthAndYear);
-				for (String s : existingList.keySet()) { //To ensure no Forex already existing but missing from this one is deleted.
+				for (String s : existingList.keySet()) {
 					listForex.put(s, existingList.get(s));
 				}	
 			}
 			SalesHistory.get().addHistoricalForex(monthAndYear, listForex);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			System.out.println("There was a problem importing this file.");
 			e.printStackTrace();
 		}
 	}
 
+	/**Parses a currency line and places the currency and its associated FX rate into the class variable listForex.
+	 * 
+	 * @param line String csv line to be parsed for currency and FX rate information.
+	 */
 	private void importForex(String line) {
+		//Divides line into its individual cells by splitting on commas that are not within quotes
+		//And trims all leading and trailing whitespace from each value.
 		String[] lineDivided = line.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)", -1);
-		double exchangeRate = 0;
+		int counter = 0;
+		for (String s : lineDivided) {
+			lineDivided[counter] = s.trim();
+			counter++;
+		}
+		
+		//Uses the value in parenthesis in the first cell as the currency
 		String currency = "";
-		exchangeRate = Double.parseDouble(lineDivided[9]) / Double.parseDouble(lineDivided[7]);
 		String[] temp = lineDivided[0].split("\\(");
 		currency = temp[1].replaceAll("[()]", "");
+		
+		//Sets the exchange rate as the division of amount paid to PLP in US dollars and the amount owed to PLP in the foreign currency,
+		//thus obtaining a more exact measure than their listed FX rate.
+		double exchangeRate = 0;
+		exchangeRate = Double.parseDouble(lineDivided[9]) / Double.parseDouble(lineDivided[7]);
+		
 		this.listForex.put(currency, exchangeRate);
 	}
 }
