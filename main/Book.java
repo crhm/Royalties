1984530cb210d7377ac4f6278dc38e6347542aae
crhm <33059858+crhm@users.noreply.github.com>
2018-02-12 16:15:56 +0000

    Added hashCode and equals methods since it is used as keys in a HashMap in SalesHistory

--- main/Book.java
+++ main/Book.java
@@ -12,7 +12,7 @@ import java.util.List;
 public class Book {
 	
 	private final String title;
-	private final String author;
+	private String author;
 	private final List<String> identifier;
 	
 	/** Book constructor. Initialises title, author and identifier to the corresponding arguments passed by the user.
@@ -31,6 +31,10 @@ public class Book {
 		return title;
 	}
 
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
 	public String getAuthor() {
 		return author;
 	}
@@ -46,6 +50,40 @@ public class Book {
 	@Override
 	public String toString() {
 		return "Book [title=" + title + ", author=" + author + ", identifier=" + identifier + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((author == null) ? 0 : author.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
+	/** Returns true if and only if the object is also a Book, with the same title and the same author.
+	 * <br>Differences in the identifiers are disregarded.
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		if (author == null) {
+			if (other.author != null)
+				return false;
+		} else if (!author.equals(other.author))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
 	}
 
 }
