f84514d7b1acfa2b33fd7e129e620dba389d6c25
crhm <33059858+crhm@users.noreply.github.com>
2018-05-05 16:31:11 +0100

    Finished reordering buttons so that they are consistent across panels (to avoid user confusion) and made their names clearer

--- gui/books/BookPanel.java
+++ gui/books/BookPanel.java
@@ -42,9 +42,9 @@ import main.SalesHistory;
 public class BookPanel extends JPanel implements ActionListener, ListSelectionListener, TableColumnModelListener {
 
 	private JTable booksTable;
-	private JButton editButton = new JButton("Edit Book");
 	private JButton addButton = new JButton("Create New Book");
-	private JButton deleteButton = new JButton("Delete Book");
+	private JButton editButton = new JButton("Edit Selected Book");
+	private JButton deleteButton = new JButton("Delete Selected Book");
 	private JButton mergeButton = new JButton("Merge together two books selected");
 	int selectedIndex1 = -1;
 	int selectedIndex2 = -1;
