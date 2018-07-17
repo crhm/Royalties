f522420c092bcb92f0cef3ca10fa4b8779662b61
crhm <33059858+crhm@users.noreply.github.com>
2018-07-17 16:07:52 +0200

    Made changes to use SalesFileFormat for sales instead of FileFormat

--- importing/ImportFactory.java
+++ importing/ImportFactory.java
@@ -2,34 +2,75 @@ package importing;
 
 import importing.forex.AmazonForexFileFormat;
 import importing.forex.AppleForexFileFormat;
-import importing.sales.AmazonFileFormat;
-import importing.sales.AppleFileFormat;
-import importing.sales.CreatespaceFileFormat;
-import importing.sales.KoboFileFormat;
-import importing.sales.NookFileFormat;
 import main.SalesHistory;
 
 public class ImportFactory implements java.io.Serializable {
 
 	private static final long serialVersionUID = 8392393437563388498L;
-	
+
 	//TODO Improve this to be less crude & more flexible (maybe by letting user distinguish whether he is selecting sales files or 
 	//FX files etc...
 
-	public static void ImportData(String fileName) {
-		FileFormat temp = null;
+	public static void ImportSales(String fileName) {
+		Boolean flagImported = false;
+		
 		if (fileName.toLowerCase().contains("kobo")) { //KOBO
-			temp = new KoboFileFormat();
+			for (SalesFileFormat sff : SalesHistory.get().getChannel("Kobo").getListSalesFileFormats()) {
+				try {
+					sff.importData(fileName);
+					flagImported = true;
+					break;
+				} catch (Exception e) {
+				}
+			}
 		} else if (fileName.toLowerCase().contains("nook")) { //NOOK
-			temp = new NookFileFormat();
+			for (SalesFileFormat sff : SalesHistory.get().getChannel("Nook").getListSalesFileFormats()) {
+				try {
+					sff.importData(fileName);
+					flagImported = true;
+					break;
+				} catch (Exception e) {
+				}
+			}
 		} else if (fileName.toLowerCase().contains("createspace")) { //CREATESPACE
-			temp = new CreatespaceFileFormat();
+			for (SalesFileFormat sff : SalesHistory.get().getChannel("Createspace").getListSalesFileFormats()) {
+				try {
+					sff.importData(fileName);
+					flagImported = true;
+					break;
+				} catch (Exception e) {
+				}
+			}
 		} else if (fileName.toLowerCase().endsWith(".txt")) { //APPLE
-			temp = new AppleFileFormat();
-		} else if (fileName.toLowerCase().contains("apple")) {
-			temp = new AppleForexFileFormat();
+			for (SalesFileFormat sff : SalesHistory.get().getChannel("Apple").getListSalesFileFormats()) {
+				try {
+					sff.importData(fileName);
+					flagImported = true;
+					break;
+				} catch (Exception e) {
+				}
+			}
+
 		} else if (fileName.toLowerCase().contains("amazon") && fileName.toLowerCase().contains("sales")) { //AMAZON
-			temp = new AmazonFileFormat();
+			for (SalesFileFormat sff : SalesHistory.get().getChannel("Amazon").getListSalesFileFormats()) {
+				try {
+					sff.importData(fileName);
+					flagImported = true;
+					break;
+				} catch (Exception e) {
+				}
+			}
+		}
+		if (flagImported) {
+			String[] pathSeparated = fileName.split("/");
+			SalesHistory.get().addImportedFile(pathSeparated[pathSeparated.length - 1]);
+		}
+	}
+
+	public static void importFX(String fileName) {
+		FileFormat temp = null;
+		if (fileName.toLowerCase().contains("apple")) {
+			 temp = new AppleForexFileFormat();
 		} else if (fileName.toLowerCase().contains("amazon") && fileName.toLowerCase().contains("payment")) {
 			temp = new AmazonForexFileFormat();
 		}
